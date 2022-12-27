6eb354396adaef1407e0335bd7834f0ac462ec8b
andresRSG <92889635+andresRSG@users.noreply.github.com>
2022-12-27 17:05:04 -0600

    Activity main Style

--- app/src/main/java/com/example/ahorcadogame/MainActivity.kt
+++ app/src/main/java/com/example/ahorcadogame/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.ahorcadogame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ahorcadogame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        listeners()
+
+    }
+
+    private fun listeners(){
+        binding.tvPlay.setOnClickListener {
+            val intent = Intent(this@MainActivity, PlayActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/ahorcadogame/PlayActivity.kt
@@ -0,0 +1,111 @@
+package com.example.ahorcadogame
+
+import android.os.Bundle
+import android.os.Vibrator
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ahorcadogame.databinding.ActivityPlayBinding
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
+class PlayActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityPlayBinding
+    private val KEYBOARD = HashMap<Int, Char>()
+    private val LOG_TAG = this.javaClass.simpleName
+    private var mDictionary: ArrayList<String>? = null
+    private val guess = 0.toChar()
+    private var flagGameOver = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPlayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        dictionary()
+
+        //Setting up the keyboard
+
+
+        //Setting up the keyboard
+        for (id in KEYBOARD.keys) {
+            val button = findViewById(id) as Button
+            button.isEnabled = true
+            button.isSoundEffectsEnabled = false
+            button.setText(
+                KEYBOARD.get(id)
+                    .toString()
+            )
+            button.setBackgroundColor(getColor(R.color.ah_yellowD))
+            button.setOnClickListener(this)
+        }
+
+
+    }
+
+    /*public void initGame() {
+        EvilHangman evilHangman = new EvilHangman(dictionary, letters);
+    }*/
+    fun updateGame(guessedLetter: Char) {
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (KEYBOARD.keys.contains(id)) {
+            val button = findViewById(id) as Button
+            button.text = ""
+            button.isEnabled = false
+//            val guessedLetter: Char = KEYBOARD.get(id)?.toChar() ?:
+//            updateGame(guessedLetter)
+        }
+    }
+
+
+    fun makeDictionary() {
+        try {
+            val wordStream = assets.open("dictionary.txt")
+            val inb = BufferedReader(InputStreamReader(wordStream))
+            var line: String? = null
+            mDictionary = ArrayList<String>()
+            while (inb.readLine().also { line = it } != null) {
+                val word = line!!.trim { it <= ' ' }
+                mDictionary!!.add(word)
+            }
+        } catch (e: IOException) {
+            Log.d(LOG_TAG, "IOException")
+        }
+    }
+
+    private fun dictionary(){
+        KEYBOARD.put(R.id.button1, 'a')
+        KEYBOARD.put(R.id.button2, 'b')
+        KEYBOARD.put(R.id.button3, 'c')
+        KEYBOARD.put(R.id.button4, 'd')
+        KEYBOARD.put(R.id.button5, 'e')
+        KEYBOARD.put(R.id.button6, 'f')
+        KEYBOARD.put(R.id.button7, 'g')
+        KEYBOARD.put(R.id.button8, 'h')
+        KEYBOARD.put(R.id.button9, 'i')
+        KEYBOARD.put(R.id.button10, 'j')
+        KEYBOARD.put(R.id.button11, 'k')
+        KEYBOARD.put(R.id.button12, 'l')
+        KEYBOARD.put(R.id.button13, 'm')
+        KEYBOARD.put(R.id.button14, 'n')
+        KEYBOARD.put(R.id.button15, 'o')
+        KEYBOARD.put(R.id.button16, 'p')
+        KEYBOARD.put(R.id.button17, 'q')
+        KEYBOARD.put(R.id.button18, 'r')
+        KEYBOARD.put(R.id.button19, 's')
+        KEYBOARD.put(R.id.button20, 't')
+        KEYBOARD.put(R.id.button21, 'u')
+        KEYBOARD.put(R.id.button22, 'v')
+        KEYBOARD.put(R.id.button23, 'w')
+        KEYBOARD.put(R.id.button24, 'x')
+        KEYBOARD.put(R.id.button25, 'y')
+        KEYBOARD.put(R.id.button26, 'z')
+    }
+
+}
